dfa4cd91da672c179d63c23f9ae2738f186a50bc
LuDi <di.lu@live.com>
2014-10-11 23:06:11 +0800

    no message

--- js/cs-modification.js
+++ js/cs-modification.js
@@ -18,25 +18,50 @@ $.ajax({
     console.log(JSON.stringify(response[0]));
 
 
-         for(var i=0; i<response.length; i++) {
+        
+          for(var i=0; i<response.length; i++) {
+            var selectId = "<td id=No"+ (i+1) + ">";
             $("#stockInfo").append(
               $('<div style="border: 1px solid black;">'),           
                 $('<tr>'),
                  $('<td>').text(i+1),
                  $('<td>').text(response[i].ric),
                  $('<td>').text(response[i].name),
-                 $('<td>').text(Math.round(response[i].livePrice*100)/100),
+                 $(selectId).text(Math.round(response[i].livePrice*100)/100),
                  $('</tr>'),
               $('</div>')
-
-                 );
+            );
           }
-
-    console.log("included.");
-
   }
 });
-//dynamic table for the stock list
+//create table for the stock list
+
+
+setInterval(function () {
+  $.ajax({
+    type: "GET",
+    url: actualUrlPrefix+ "instruments",
+    // data: data,
+    crossDomain : true,
+    success: function(data){
+      var response = $.parseJSON(data);
+      console.log(response);
+      console.log(response[0]);
+      console.log(JSON.stringify(response[0]));
+
+
+      for(var i=0; i<response.length; i++){
+        var selectId="No"+ (i+1) ;
+        $('#'+selectId).text(Math.round(response[i].livePrice*100)/100)
+      }
+      //$('#No1').text(Math.round(response[0].livePrice*100)/100)
+    }
+  });
+}, 1000);
+//dynamic update the live price
+
+
+
 
 //search in the stock list
 var $rows = $('#stockInfo tr');
