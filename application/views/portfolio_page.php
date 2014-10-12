6a8b4129b9ea549425f1eedeeb754419768ede43
LuDi <di.lu@live.com>
2014-10-12 11:32:51 +0800

    secondpage

--- application/views/portfolio_page.php
+++ application/views/portfolio_page.php
@@ -21,6 +21,33 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+    type="text/javascript"></script>
+
+    <style type="text/css">
+    @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+
+    #feedControl {
+    margin-top : 10px;
+    margin-left: auto;
+    margin-right: auto;
+    width : 440px;
+    font-size: 12px;
+    color: #9CADD0;
+    }
+    </style>
+    <script type="text/javascript">
+    function load() {
+    var feed ="http://feeds.bbci.co.uk/news/world/rss.xml";
+    new GFdynamicFeedControl(feed, "feedControl");
+
+    }
+    google.load("feeds", "1");
+    google.setOnLoadCallback(load);
+    </script>
+    <!--Google News Config-->
+
 </head>
 
 <body>
@@ -84,7 +111,7 @@
             <!-- /.row -->
             
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-8">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Portfolio Analysis
@@ -92,70 +119,49 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="flot-chart">
-                                <div class="flot-chart-content" id="flot-pie-chart"></div>
+                                <div id="pieChart"></div>
                             </div>
                         </div>
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
                 </div>
-       
-                <div class="pull-right">    
-                    <div class="col-lg-5">
-                        <div class="panel-body">
-                            <div class="row">
-                                <input type="text" id="search" placeholder="Search for Stock" style="width:400px">
-                                    <table class="table table-bordered table-hover table-striped" id="stockInfoHead" style="width:400px">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>StockNo.</th>
-                                                <th>CompanyName</th>
-                                                <th>LivePrice</th>
-                                           </tr>
-                                        </thead>
-                                     </table>
-                                <div style="width:400px;height:440px;overflow:scroll;">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered table-hover table-striped" id="stockInfo">
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.row -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Related News
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <a href="http://feeds.finance.yahoo.com/rss/2.0/headline?s="+
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <div class="col-lg-6">
+
+                <div class="col-lg-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Social Footprint
+                            <a class="twitter-timeline" href="https://twitter.com/hashtag/finance" data-widget-id="521135888940556291">#finance Tweets</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>    
                         </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            
-                        </div>
-                        <!-- /.panel-body -->
+                        
                     </div>
                     <!-- /.panel -->
                 </div>
+
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Related News
+                                <div id="body">
+                                    <div id="feedControl">Loading...</div>
+                                </div>
+                            </div>
+                            <!-- /.panel-heading -->
+                            <div class="panel-body">
+                                <a href="http://feeds.finance.yahoo.com/rss/2.0/headline?s="+
+                            </div>
+                            <!-- /.panel-body -->
+                        </div>
+                        <!-- /.panel -->
+                    </div>
+                    
+                </div>
+
             </div>
+            
         </div>
         <!-- /#page-wrapper -->
     </div>
@@ -176,4 +182,54 @@
     <script src="js/cs-modification.js"></script>
 </body>
 
+
+<style>
+    #pieChart {
+        width       : 600px;
+        height      : 600px;
+        font-size   : 11px;
+        margin-bottom: 0px;
+        margin-left  : 0px;
+        margin-right : 0px;
+        margin-top   : 0px;
+        padding     : 0px;
+    }
+</style>
+<script type="text/javascript" src="http://www.amcharts.com/lib/3/amcharts.js"></script>
+<script type="text/javascript" src="http://www.amcharts.com/lib/3/pie.js"></script>
+<script type="text/javascript" src="http://www.amcharts.com/lib/3/themes/none.js"></script>
+<script type="text/javascript" src="http://underscorejs.org/underscore-min.js"></script>
+<script type="text/javascript">
+
+var pieDataArray1 = ["Lithuania","Czech Republic","Ireland","Germany"];
+var pieDataArray2 = [100,200,300,400];
+var pieData = [];
+for (var i=0; i<pieDataArray2.length; i++){
+    pieData.push({ric: pieDataArray1[i], allowance: pieDataArray2[i]});
+}
+
+var pieChart = AmCharts.makeChart("pieChart", {
+    "type": "pie",
+    "theme": "none",
+    "dataProvider": pieData,
+    "valueField": "allowance",
+    "titleField": "ric",
+    "exportConfig":{    
+      menuItems: [{
+      icon: '/lib/3/images/export.png',
+      format: 'png'   
+      }]  
+    }
+});
+</script>
+<!--Pie Chart-->
+
+
+
+
+
 </html>
+
+
+
+
