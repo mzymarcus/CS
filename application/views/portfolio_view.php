bd3dcc7901a7ef9e1f10c2cc57c082792a9f3e1a
Haibin Lin <linhaibin.eric@gmail.com>
2014-10-12 09:19:11 +0800

    pie chart

--- application/views/portfolio_view.php
+++ application/views/portfolio_view.php
@@ -3,39 +3,31 @@
 <div id='chartdiv'> </div>
 
 <style>
-#chartdiv {
-    width       : 100%;
-    height      : 500px;
-    font-size   : 11px;
-}
+    #chartdiv {
+        width       : 100%;
+        height      : 300px;
+        font-size   : 11px;
+    }
 </style>
 <script type="text/javascript" src="http://www.amcharts.com/lib/3/amcharts.js"></script>
 <script type="text/javascript" src="http://www.amcharts.com/lib/3/pie.js"></script>
 <script type="text/javascript" src="http://www.amcharts.com/lib/3/themes/none.js"></script>
-
-
-
+<script type="text/javascript" src="http://underscorejs.org/underscore-min.js"></script>
 <script type="text/javascript">
-var chart = AmCharts.makeChart("chartdiv", {
+
+var pieDataArray1 = ["Lithuania","Czech Republic","Ireland","Germany"];
+var pieDataArray2 = [100,200,300,400];
+var pieData = [];
+for (var i=0; i<pieDataArray2.length; i++){
+    pieData.push({ric: pieDataArray1[i], allowance: pieDataArray2[i]});
+}
+
+var pieChart = AmCharts.makeChart("chartdiv", {
     "type": "pie",
     "theme": "none",
-    "dataProvider": [
-{
-        "country": "Lithuania",
-        "litres": 501.9
-    },
-{
-        "country": "Czech Republic",
-        "litres": 301.9
-    }, {
-        "country": "Ireland",
-        "litres": 201.1
-    }, {
-        "country": "Germany",
-        "litres": 165.8
-    }],
-    "valueField": "litres",
-    "titleField": "country",
+    "dataProvider": pieData,
+    "valueField": "allowance",
+    "titleField": "ric",
     "exportConfig":{    
       menuItems: [{
       icon: '/lib/3/images/export.png',
